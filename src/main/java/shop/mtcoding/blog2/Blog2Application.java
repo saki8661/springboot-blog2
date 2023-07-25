02294ff7598a38ca713aa87f59869f0251c38474
saki8661 <saki8661@naver.com>
2023-07-26 01:07:33 +0900

    yml파일 수정, 실행시 한글 컨버팅 에러

--- src/main/java/shop/mtcoding/blog2/Blog2Application.java
+++ src/main/java/shop/mtcoding/blog2/Blog2Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Blog2Application {
 
 	public static void main(String[] args) {
+		System.out.println("hello 안녕");
 		SpringApplication.run(Blog2Application.class, args);
 	}
 
