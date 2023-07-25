22371ae3a2caff445300e414ad7a9c41b86e2e85
saki8661 <saki8661@naver.com>
2023-07-26 00:49:11 +0900

    blog2생성

---
+++ src/test/java/shop/mtcoding/blog2/Blog2ApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.blog2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Blog2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
